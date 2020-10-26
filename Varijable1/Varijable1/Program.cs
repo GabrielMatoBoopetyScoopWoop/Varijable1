4c185648902e656acdb012e92680b378fd94a503
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2020-10-26 09:19:56 +0100

    Add files via upload

---
+++ Varijable1/Varijable1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float ProsjekOcjena = 0;
+            for (int Ocjena = 1, i = 1; Ocjena != 0; i++)
+            {
+                Console.Write("Upisite ocjenu:");
+                Ocjena = Convert.ToInt32(Console.ReadLine());
+                if (Ocjena <= 5 && Ocjena >= 0)
+                {
+                    if (Ocjena == 0)
+                    {
+                        Console.Write("Prosjek ocjena:" + ProsjekOcjena / (i-1));
+                    }
+                    else ProsjekOcjena += Ocjena;
+                }
+                else
+                {
+                    Console.WriteLine("Krivi unos!");
+                    i--;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
